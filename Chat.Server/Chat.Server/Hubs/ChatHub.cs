b5f7e0fe52327cb2451897d2fa2bc21a21de8ddb
Zafarbek <=>
2023-07-13 10:11:09 +0500

    first

---
+++ Chat.Server/Chat.Server/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Server.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Server.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task GetMessage(Message message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ Chat.Server/Chat.Server/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Chat.Server.Models
+{
+    public class Message
+    {
+        public Guid MessageId { get; set; }
+        public string UserName { get; set; }
+        public string Body { get; set; }
+        public DateTime SendTime { get; set; }
+    }
+}
